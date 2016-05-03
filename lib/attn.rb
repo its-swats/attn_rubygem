441934b09d4fa37be9b04ca9a170a04a9840cd15
Shawn Watson <shawnw86@gmail.com>
2016-05-03 13:49:08 -0700

    Add color selection for procs

--- lib/attn.rb
+++ lib/attn.rb
@@ -1,9 +1,20 @@
-def attn(method = nil)
+def attn(method = nil, color = 'yellow')
+	colors = {'red' => '031','green' => '032','yellow' => '033','blue' => '034','magenta' => '035','cyan' => '036'}
+	color = method, method = nil if colors.keys.include?(method)
+	color = 'yellow' unless colors.keys.include?(color)
+
   unless method
-    print "\033[43m* * * * * * * * * * * * * * * * * * * *\033[0m\n"
+    print "\033[#{colors[color]}m* * * * * * * * * * * * * * * * * * * * * * * * *\033[0m\n"
   else
-    print "\033[43m* * * * * * * * * * * * * * * * * * * *\033[0m\n"
-    p method
-    print "\033[43m* * * * * * * * * * * * * * * * * * * *\033[0m\n"
+  	proc = Proc.new { method }
+    print "\033[#{colors[color]}m* * * * * * * * * * START * * * * * * * * * * * *\033[0m\n"
+    p proc.call
+    print "\033[#{colors[color]}m* * * * * * * * * * FINISH  * * * * * * * * * * *\033[0m\n"
   end
+
 end
+
+
+# attn('test', 'red')
+attn('asdred', 'red')
+# attn
