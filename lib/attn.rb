86b2bb69d46017004c667724a8baa9fe09a53e80
Shawn Watson <shawnw86@gmail.com>
2016-05-03 14:32:58 -0700

    Update spacing

--- lib/attn.rb
+++ lib/attn.rb
@@ -3,14 +3,14 @@ def attn(method = nil, color = 'yellow')
 
 	color,method = method,nil if colors.keys.include?(method)
 	color = 'yellow' unless colors.keys.include?(color)
-		
-  unless method
-    print "\033[#{colors[color]}m* * * * * * * * * * * * * * * * * * * * * * * * *\033[0m\n"
-  else
-  	proc = Proc.new { method }
-    print "\033[#{colors[color]}m* * * * * * * * * * START * * * * * * * * * * * *\033[0m\n"
-    p proc.call
-    print "\033[#{colors[color]}m* * * * * * * * * * FINISH  * * * * * * * * * * *\033[0m\n"
-  end
+	
+	unless method
+		print "\033[#{colors[color]}m* * * * * * * * * * * * * * * * * * * * * * * * *\033[0m\n"
+	else
+		proc = Proc.new { method }
+		print "\033[#{colors[color]}m* * * * * * * * * * START * * * * * * * * * * * *\033[0m\n"
+		p proc.call
+		print "\033[#{colors[color]}m* * * * * * * * * * FINISH  * * * * * * * * * * *\033[0m\n"
+	end
 
 end
