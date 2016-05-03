7c919bbeeb24fa7659fc918d09985ae63d7c139b
Shawn Watson <shawnw86@gmail.com>
2016-05-03 14:02:16 -0700

    Update colors to allow for single line

--- lib/attn.rb
+++ lib/attn.rb
@@ -1,8 +1,9 @@
 def attn(method = nil, color = 'yellow')
 	colors = {'red' => '031','green' => '032','yellow' => '033','blue' => '034','magenta' => '035','cyan' => '036'}
-	color = method, method = nil if colors.keys.include?(method)
-	color = 'yellow' unless colors.keys.include?(color)
 
+	color,method = method,nil if colors.keys.include?(method)
+	color = 'yellow' unless colors.keys.include?(color)
+		
   unless method
     print "\033[#{colors[color]}m* * * * * * * * * * * * * * * * * * * * * * * * *\033[0m\n"
   else
@@ -13,8 +14,3 @@ def attn(method = nil, color = 'yellow')
   end
 
 end
-
-
-# attn('test', 'red')
-attn('asdred', 'red')
-# attn
