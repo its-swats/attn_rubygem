d5d4655f097799155d0cb99f0a37a9368036243a
BootCoder <BootCoder@gmail.com>
2017-09-07 13:43:36 -0700

    disable coloring for now - switch out * for ⬇

--- lib/attn.rb
+++ lib/attn.rb
@@ -1,5 +1,5 @@
-require 'awesome_print'
 require 'colorize'
+require 'awesome_print'
 
 $COLORS = {'red' => '031','green' => '032','yellow' => '033','blue' => '034','magenta' => '035','cyan' => '036'}
 
@@ -9,36 +9,48 @@ def attn(variable = nil, color = '')
 	color = $COLORS.keys.sample unless $COLORS.keys.include?(color)
 
 	unless variable
-		puts "\033[#{$COLORS[color]}m* * * * * * * * * * * * * * * * * * * * * * * * *\033[0m\n"
+		puts star_break(color)
 	else
-		proc = Proc.new { variable }
-		detail_report(variable, color)
-		ap proc.call
-		puts "\033[#{$COLORS[color]}m* * * * * * * * * * FINISH  * * * * * * * * * * *\033[0m"
-		puts String.colors
-		puts
+		puts detail_report(variable, color)
+		display_variable(variable)
+		puts close_report(color)
 	end
 
 end
 
 private
 
+def display_variable(variable)
+	proc = Proc.new { variable }
+	puts
+	ap proc.call
+	puts
+end
+
+def star_break(color)
+	""
+end
+
+def close_report(color)
+	str = "⬆ " * 15
+	str += " FINISH "
+	str += "⬆ " * 15
+end
+
 def detail_report(variable, color)
-	print "* * * * * ".send(color)
+	str = "\n"
+	str += "⬇ " * 5
 
-	print "Object:".send(color)
-	print " #{variable} ".green
+	str += " Class:"
+	str += " #{variable.class} ".send(color)
 
-	print "Class:".send(color)
-	print " #{variable.class} ".green
+	str += "Length:" if variable.respond_to?(:length)
+	str += " #{variable.length} ".send(color) if variable.respond_to?(:length)
 
-	print "Length:".send(color) if variable.respond_to?(:length)
-	print " #{variable.length} ".green if variable.respond_to?(:length)
+	str += "Caller:"
+	str += " #{caller[-1]} ".send(color)
 
-	print "Caller".send(color)
-	print " #{caller[1]}".green
-
-	print " * * * * *\n".send(color)
+	str += "⬇ " * 5
 end
 
 
