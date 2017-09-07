4e893ea042a71ebba97668aedb8195e85c899dc5
BootCoder <BootCoder@gmail.com>
2017-09-07 15:36:28 -0700

    refactor to use instance vars instead of passing args

--- lib/attn.rb
+++ lib/attn.rb
@@ -10,17 +10,18 @@ def attn(variable = nil, color = '')
 	unless variable
 		face_break
 	else
-		detail_report(variable, color)
-		display_variable(variable)
-		close_report(color)
+		@color,@variable = color,variable
+		detail_report
+		display_variable
+		close_report
 	end
 
 end
 
 private
 
-def display_variable(variable)
-	proc = Proc.new { variable }
+def display_variable
+	proc = Proc.new { @variable }
 	puts
 	p proc.call
 	puts
@@ -31,30 +32,30 @@ def face_break
 	puts "#{chars.sample}  " * 40
 end
 
-def close_report(color)
+def close_report
 	str = "⬆ " * 15
 	str += " FINISH "
 	str += "⬆ " * 15
-	puts str.send(color)
+	puts str.send(@color)
 end
 
-def detail_report(variable, color)
-	secondary_color = color == 'red' ? 'blue' : 'red'
+def detail_report
+	secondary_color = @color == 'red' ? 'blue' : 'red'
 	str = "\n"
-	str += "⬇ ".send(color) * 5
+	str += "⬇ ".send(@color) * 5
 
-	str += " CLASS:".send(color)
-	str += " #{variable.class} ".send(secondary_color).bold
+	str += " CLASS:".send(@color)
+	str += " #{@variable.class} ".send(secondary_color).bold
 
-	if variable.respond_to?(:length)
-		str += "LENGTH:".send(color)
-		str += " #{variable.length} ".send(secondary_color).bold
+	if @variable.respond_to?(:length)
+		str += "LENGTH:".send(@color)
+		str += " #{@variable.length} ".send(secondary_color).bold
 	end
 
-	str += "CALLER:".send(color)
+	str += "CALLER:".send(@color)
 	str += " #{caller[-1]} ".send(secondary_color).bold
 
-	str += "⬇ ".send(color) * 5
+	str += "⬇ ".send(@color) * 5
 	puts str
 end
 
